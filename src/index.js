832b77e503bc4931a4af9e46c7f61dcc9ad3e64e
nstppbl <1nstppbl@gmail.com>
2021-02-18 20:12:52 +0500

    feat: add functions for max min avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+    if (!array || !array.length) {
+        return 0 }
+        
+    let minm = 0
+    for (let i = 0; i < array.length; i++){
+        if (array[i] < minm) {
+            minm = array[i] 
+        }  
+        }   
+    return minm 
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if (!array || !array.length) {
+        return 0 }
+
+    let maxm = 0
+    for (var i = 0; i < array.length; i++){
+        if (array[i] > maxm) {
+            maxm = array[i]
+        }
+    }
+    return maxm 
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    if (!array || !array.length) {
+        return 0 }
+
+    let sum = 0
+    for (var i = 0; i < array.length; i++){
+            sum = sum + array[i]
+        }
+    return (sum/array.length)
 }
